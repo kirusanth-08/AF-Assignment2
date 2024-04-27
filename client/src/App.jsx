837da0711baf3a174adb80deb73df4638968451c
Kirusanth <it21838170@my.sliit.lk>
2024-04-28 00:07:59 +0530

    Add ImageDetail component and update dependencies in App.jsx and package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Menu from './components/Menu/Menu';
 import MarsRover from './components/MarsRover/MarsRover';
+import ImageDetail from './components/ImageDetail/ImageDetail';
 
 const App = () => (
   <div className="App">
@@ -11,6 +12,7 @@ const App = () => (
     <Home />
     <Menu />
     <MarsRover />
+    <ImageDetail /> 
   </div>
 );
 
---
+++ client/src/components/ImageDetail/ImageDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageDetail = () => {
+  return (
+    <div className='w-full bg-white-400 h-screen flex justify-center items-center'>
+        <div className='w-4/5 h-3/4 bg-black-400 rounded-2xl overflow-hidden shadow-inner'></div>
+    </div>
+  )
+}
+
+export default ImageDetail

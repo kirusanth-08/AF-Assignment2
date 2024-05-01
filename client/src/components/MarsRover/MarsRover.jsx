70551e3320109f27c0a923d8c6d0766a67556f70
Kirusanth <it21838170@my.sliit.lk>
2024-05-01 23:30:13 +0530

    Refactor component imports, add MarsRover component, and update dependencies

--- client/src/components/MarsRover/MarsRover.jsx
+++ client/src/components/MarsRover/MarsRover.jsx
@@ -1,29 +1,97 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { React, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const MarsRover = () => {
-  return (
-    <div className='bg-blurred w-full h-screen flex justify-center items-center'>
-        <div className=' rounded-lg p-5 bg-opacity-20 bg-white border-2 border-white'>
-          <form className='flex flex-col items-start justify-center'>
-            <label className='text-2xl font-bold'>Mars Rover Photos</label>
-            <div>
-              <label>Earth Date:</label>
-              <input type='date' className='p-2' />
-            </div>
-            <div>
-              <label>Select Rover:</label>
-              <select className='w-1/2 p-2 m-2'>
-                <option value='curiosity'>Curiosity</option>
-                <option value='opportunity'>Opportunity</option>
-                <option value='spirit'>Spirit</option>
-              </select>
-            </div>
-            <Link to='/photos' className='w-1/2 p-2 m-2 bg-black text-white hover:bg-opacity-50'>Search</Link>
-          </form>
-        </div>
-    </div>
-  )
-}
+  const [selectedRover, setSelectedRover] = useState("curiosity");
+  const [selectedCamera, setSelectedCamera] = useState("rhaz");
+  const [selectedSol, setSelectedSol] = useState(1);
+  const [selectedEarthDate, setSelectedEarthDate] = useState("2013-04-25");
+  const [images, setImages] = useState([]);
 
-export default MarsRover
+  const handleRoverChange = (event) => {
+    setSelectedRover(event.target.value);
+  };
+
+  const handleCameraChange = (event) => {
+    setSelectedCamera(event.target.value);
+  };
+
+  const handleSolChange = (event) => {
+    setSelectedSol(event.target.value);
+  };
+
+  const handleEarthDateChange = (event) => {
+    setSelectedEarthDate(event.target.value);
+  };
+
+
+useEffect(() => {
+  axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${selectedRover}/photos?sol=${selectedSol}&api_key=rXQUrs3QIGnJgbZxTAJy0P48RGpR8F7f3mqwLJWJ&camera=${selectedCamera}&earth_date=${selectedEarthDate}`)
+    .then(response => {
+      setImages(response.data.photos);
+    })
+    .catch(error => console.error(error));
+}, [selectedRover, selectedCamera, selectedSol, selectedEarthDate]);
+  return (
+    <div className="bg-blurred w-full h-screen flex justify-center items-center">
+      <div className=" rounded-lg p-5 bg-opacity-20 bg-white border-2 border-white">
+        <div className="flex flex-col items-start justify-center">
+          <label className="text-2xl font-bold">Mars Rover Photos</label><br />
+          <div>
+            <label>Earth Date:</label>
+            <input
+              type="date"
+              className="p-2"
+              value={selectedEarthDate}
+              onChange={handleEarthDateChange}
+            />
+          </div>
+          <div className="w-full">
+            <label>Sol:</label>
+            <input
+              type="number"
+              className="p-2 text-right"
+              value={selectedSol}
+              onChange={handleSolChange}
+            />
+          </div>
+          <div>
+            <label>Select Camera:</label>
+            <select
+              className="w-1/2 p-2 m-2 bg-black bg-opacity-25 rounded-lg"
+              value={selectedCamera}
+              onChange={handleCameraChange}
+            >
+              <option value="fhaz">Front Hazard Avoidance Camera</option>
+              <option value="rhaz">Rear Hazard Avoidance Camera</option>
+              <option value="mast">Mast Camera</option>
+              <option value="chemcam">Chemistry and Camera Complex</option>
+              <option value="mahli">Mars Hand Lens Imager</option>
+              <option value="mardi">Mars Descent Imager</option>
+            </select>
+          </div>
+          <div>
+            <label>Select Rover:</label>
+            <select
+              className="w-1/2 p-2 m-2 bg-black bg-opacity-25 rounded-lg"
+              value={selectedRover}
+              onChange={handleRoverChange}
+            >
+              <option value="curiosity">Curiosity</option>
+              <option value="opportunity">Opportunity</option>
+              <option value="spirit">Spirit</option>
+            </select>
+          </div>
+          <Link to={{
+            pathname: '/photos',
+            state: { images }
+          }} className='w-full p-2 m-2 bg-black text-white hover:bg-opacity-50 text-center rounded-lg'
+          >Get photos ({ images.length })</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarsRover;
--- client/src/components/Photos.jsx
+++ client/src/components/Photos.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Photos = () => {
+  const location = useLocation();
+  const images = location.state?.images || [];
+
+  console.log(images);
+
   return (
-    <div classname=" bg-blurred h-screen w-full">
-        <h1 className='text-2xl font-bold'>Mars Rover Photos</h1>
-        <div className='grid grid-cols-2 gap-4 center'>
-            <img src='https://spaceplace.nasa.gov/gallery-space/en/NGC2336-galaxy.en.jpg' alt='mars' className='w-full h-full object-cover' />
-        </div>
+    <div className="bg-blurred h-screen w-full">
+      <div className='grid grid-cols-2 gap-4 center'>
+        {images.map((image, index) => (
+          <img key={index} src={image.img_src} alt={image.rover.name} className='w-full h-full object-cover' />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Photos
+export default Photos;

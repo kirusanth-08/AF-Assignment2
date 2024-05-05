2ffb1c97aa4038b49aff176006f2edbd3bdb4d02
Kirusanth <it21838170@my.sliit.lk>
2024-05-05 13:05:15 +0530

    chore: Update userRoutes.js to use GET method for root route

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/auth');
 
-router.post('/', console.log("Hello"));
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the user route' });
+});
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/authenticate', authMiddleware.authenticate);
